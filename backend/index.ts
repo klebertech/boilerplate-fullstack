f0fa9cae07a438787b11cd4d49fa6919ae502b7a
Kleber <kleberst88@gmail.com>
2023-05-03 01:35:27 -0300

    fix: Add listen do server

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
 
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.status(200).send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port 3000');
 });
